11d1be1f9e81ff6f27239c68faba4365a6332402
Elana Strijbis <ehgestrijbis@gmail.com>
2022-06-17 13:44:22 +1000

    commit -1

--- nav.js
+++ nav.js
@@ -1,5 +1,5 @@
-let header = document.querySelector('header')
+// let nav = document.querySelector('navbar')
 
-window.addEventListener('scroll', () => {
-  header.classList.toggle('sticky', window.scrollY > 0)
-})
+// window.addEventListener('scroll', () => {
+//   nav.classList.toggle('sticky', window.scrollY > 0)
+// })
---
+++ script.js
@@ -0,0 +1,10 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
+(".navbar-links").click(function(){
+    navbarLinks.classList.toggle('active')
+});
